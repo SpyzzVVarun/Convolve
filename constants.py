5b76d354d6ec3f052782e687b974e5db329a2c58
Varun Nagpal <95134445+SpyzzVVarun@users.noreply.github.com>
2023-01-03 11:55:02 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,2 @@
+driver_path = "chromedriver.exe"
+brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
---
+++ parsing.py
@@ -0,0 +1,60 @@
+import warnings
+warnings.filterwarnings('ignore')
+from driver import driver
+from links import links
+from selenium.webdriver.common.by import By
+import re
+import json
+
+feature_configs = dict()
+
+for link in links:
+    driver.get(link)
+    elements = driver.find_elements(By.XPATH, "//*[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'summary steps')]")
+    if (len(elements)):
+        for element in elements:
+            headings = []
+            for i in range(1,7):
+                try:
+                    headings.append(element.find_element(By.XPATH, f"./preceding::h{i}[1]").location["y"])
+                except:
+                    pass
+            idx = headings.index(max(headings))+1
+            heading = element.find_element(By.XPATH, f"./preceding::h{idx}[1]")
+            ol = element.find_element(By.XPATH, "./following-sibling::ol[1]") #assumption
+            if (ol.text.split('\n')[-1] == ol.text.split('\n')[-2]):
+                feature_configs[heading.text] = ol.text.split('\n')[:-1]
+            else:
+                feature_configs[heading.text] = ol.text.split('\n')
+    else:
+        elements = driver.find_elements(By.CSS_SELECTOR, "table")
+        if (len(elements)):
+            for element in elements:
+                headings = []
+                for i in range(1,7):
+                    try:
+                        headings.append(element.find_element(By.XPATH, f"./preceding::h{i}[1]").location["y"])
+                    except:
+                        pass
+                idx = headings.index(max(headings))+1
+                heading = element.find_element(By.XPATH, f"./preceding::h{idx}[1]")
+                if 'procedure' in heading.text.lower():
+                    headings[idx-1] = -1
+                    idx = headings.index(max(headings))+1
+                    heading = element.find_element(By.XPATH, f"./preceding::h{idx}[1]")
+                    
+                if element.get_attribute("outerHTML").lower().count('step') >= 3:
+                    lst = []
+                    for row in element.find_elements(By.CSS_SELECTOR, "tr")[1:]:
+                        if row.text.lower().split(' ')[0] == 'note':
+                            continue
+                        string = row.find_elements(By.CSS_SELECTOR, "td")[1].text.lower()
+                        word = "example"
+                        result = string.split(word)[0]
+                        lst.append(re.sub("\n", "", result))
+                    feature_configs[heading.text] = lst
+
+    json_object = json.dumps(feature_configs, indent = 4) 
+    with open("trial.json", "w") as f:
+        f.write(json_object)
+        f.close()
