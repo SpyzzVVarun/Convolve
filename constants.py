e3dc06f6fb0d4bd56ddeb8032f9a2b8511aff3f1
Varun Nagpal <95134445+SpyzzVVarun@users.noreply.github.com>
2023-01-04 22:31:35 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,2 @@
-driver_path = "chromedriver.exe"
-brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 black_listed = ['http://www.cisco.com/c/en/us/td/docs/general/whatsnew/whatsnew.html', 'http://www.cisco.com/go/cfn', 'https://cfnng.cisco.com/', 'https://tools.cisco.com/Support/CLILookup', 'https://www.cisco.com/c/en/us/td/docs/ios-xml/ios/mcl/allreleasemcl/all-book.html', 'http://www.cisco.com/go/mibs', 'https://www.cisco.com/c/en/us/support/index.html']
 
