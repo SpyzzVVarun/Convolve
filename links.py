0aaefa4b4dbe8bb32afe8cccb9f6d5bc5fc391ff
aryanlath <99634099+aryanlath@users.noreply.github.com>
2023-01-04 21:56:01 +0530

    Update links.py

--- links.py
+++ links.py
@@ -3,14 +3,17 @@ from selenium.webdriver.common.by import By
 from constants import black_listed
 
 def search_page(link, landing_links):
-    if link != None: driver.get(link)
+    try:
+        if link != None: driver.get(link)
+    except:
+        pass
     further_links = []
     try:
         page_search_area = driver.find_element(By.ID, "pageContentDiv")
         psa = page_search_area.find_elements(By.TAG_NAME, "a")
         for lin in psa:
             lnk = lin.get_attribute("href")
-            if lnk != link and (lnk != None) and not (lnk in black_listed) and not('#' in lnk) and not(lnk in landing_links):
+            if lnk != link and (lnk != None) and not (lnk in black_listed) and not ('#' in lnk) and not (lnk in landing_links) and not ('.jpg' in lnk) and not ('github' in lnk) and not ('/tools.' in lnk) and not ('/mcl/' in lnk) and not ('.ciscosystems.' in lnk) and not ('community.cisco'in lnk) and not ('/mibs' in lnk) and not ('Plug-and-Play' in lnk) and not('cfnng.cisco.com' in lnk):
                 further_links.append(lnk)
     except:
         pass
@@ -32,7 +35,17 @@ def search_page(link, landing_links):
 
 def get_landing_links(start_page_link):
 
+#     driver.get(start_page_link)
+#     
+#     ul = driver.find_element(By.ID, "bookToc") #assumption
+#     lis = ul.find_elements(By.TAG_NAME, "li")
+#     for li in lis:
+#         links = li.find_elements(By.TAG_NAME, "a")
+#         for link in links:
+#             index_links.append(link.get_attribute("href"))
+
     index_links = []
+    hist = []
     landing_page_links = []
     index_links.append(start_page_link)
 
@@ -45,12 +58,25 @@ def get_landing_links(start_page_link):
             if(landing_page):
                 landing_page_links.append(link)
             else:
-                 new_links += further_links
+                for lk in further_links:
+                    if lk in hist:
+                        further_links.remove(lk)
+                    else:
+                        hist.append(lk)
+#                         further_links = set(further_links)
+#                         further_links.discard(lk)
+#                         further_links = list(further_links)
+#                 hist += further_links
+                new_links += further_links
+                # print(further_links)
+                # print("      ")
+                # print(hist)
+                # print("      ")
 
         index_links = new_links.copy()
 
 
         if(len(index_links) == 0):
             break
-
+    # print(len(hist))
     return landing_page_links
